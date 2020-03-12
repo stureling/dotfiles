6fcb5dcedfcc2193b9d4ebbb227862df80b4fabf
Eric Jönsson <eric.joensson.sarv@gmail.com>
2020-03-12 19:23:56 +0100

    added more config files

---
+++ scripts/launchPolybar.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Terminate already running bar instances
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# Launch bar
+if [[ $(hostname) == 'saturnus' ]]; then
+    polybar top  &
+
+elif [[ $(hostname) == 'jupiter' ]]; then
+    #TODO Add the polybar code for jupiter
+fi
+
+echo "Bars launched..."
